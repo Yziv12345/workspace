c6f557c5b7ee69bba93303262a3eff9732a94dcf
Yehonatan Ziv <yehonatan.ziv@gmail.com>
2019-02-04 14:29:50 +0200

    a4-2-19

---
+++ BackendStack/npm_exc_1.js
@@ -0,0 +1,35 @@
+//TODO: Create a new project(or use an existing one)
+//TODO: Init as an npm project
+//TODO: make sure to add node_modules to .gitignore (we do not want to push the dependencies to our repo, only the package.json and package-lock.json)
+//TODO: push to github
+//TODO: install jquery, bootstrap, cheerio moment.js and jsdom -- go over the docs of each to understand their purpose
+// - https://www.npmjs.com/package/markdown
+// - https://github.com/cheeriojs/cheerio
+// - https://www.npmjs.com/package/bootstrap
+// - https://www.npmjs.com/package/jquery
+// - https://www.npmjs.com/package/jquery-ui
+// - https://www.npmjs.com/package/moment
+// - https://www.npmjs.com/package/jsdom
+// - Make sure that json-server is installed globally
+//TODO: Use the dependencies (some are for the client, some are for the server)
+//TODO: clone the repo and run npm install -- make sure everything still works and that the node_modules directory has been created
+//TODO: see the changes in your package.json
+//TODO: use npm list, npm list -g, and the depth flag -- npm list -g depth=0
+
+//Some more cool npm packages:
+//https://www.npmjs.com/package/nodejs-base64
+//https://www.npmjs.com/package/md5
+//https://www.npmjs.com/package/uuid
+//https://www.npmjs.com/package/randomstring
+//https://www.npmjs.com/package/pluralize
+//
+//https://www.npmjs.com/package/when
+//https://www.npmjs.com/package/async
+//
+//https://www.npmjs.com/package/request
+//https://www.npmjs.com/package/node-fetch
+//
+//https://www.npmjs.com/package/underscore
+//https://www.npmjs.com/package/axios
+
+//https://github.com/marak/Faker.js/
